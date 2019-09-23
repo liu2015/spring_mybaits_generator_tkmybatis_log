2d539f3cc666c31903bc16d3e14a16e893778081
ZHY <liufuling14@163.com>
2019-09-23 10:57:40 +0800

    新的配置分支，可以省去很多配置时间

---
+++ src/main/java/com/example/demo/mapper/JiaoseMapper.java
@@ -0,0 +1,7 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.Jiaose;
+import tk.mybatis.MyMapper;
+
+public interface JiaoseMapper extends MyMapper<Jiaose> {
+}
---
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.User;
+import tk.mybatis.MyMapper;
+
+public interface UserMapper extends MyMapper<User> {
+}
