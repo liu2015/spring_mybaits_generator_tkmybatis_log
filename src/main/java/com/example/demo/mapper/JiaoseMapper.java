8115e3e71cf0fb803c6bab9c56b6574e912c7c11
ZHY <liufuling14@163.com>
2019-09-24 09:01:21 +0800

    新的配置分支，可以省去很多配置时间2 卡在shiro了122

---
+++ src/main/java/com/example/demo/mapper/JiaoseMapper.java
@@ -0,0 +1,6 @@
+package com.example.demo.mapper;
+
+import tk.mybatis.MyMapper;
+
+public interface JiaoseMapper extends MyMapper<Jiaose> {
+}
---
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,6 @@
+package com.example.demo.mapper;
+
+import tk.mybatis.MyMapper;
+
+public interface UserMapper extends MyMapper<User> {
+}
